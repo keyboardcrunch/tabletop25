e129d599dd19348a80ae6796885ca0f80b320606
keyboardcrunch <frank@keybaordcrunch>
2025-04-12 15:26:49 -0700

    C# -> Pwsh Crash reporter

---
+++ CrashReporter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+class CrashReporter
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            String cmd = "Get-ChildItem -Recurse -Path $env:LOCALAPPDATA -Filter *.zip | Out-File -Append -FilePath ~/Desktop/enum.txt; Sleep 50000";
+            
+            ps.AddScript(cmd);
+            ps.Invoke();
+            rs.Close();
+        }
+        
+    }
+}
