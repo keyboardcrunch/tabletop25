3f259fa58d41ff9b823c8f7d04858e07357f5a4e
keyboardcrunch <frank@keybaordcrunch>
2025-04-14 12:57:51 -0700

    updated BeaverUpdate with websocket

--- BeaverUpdate/Program.cs
+++ BeaverUpdate/Program.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Net.WebSockets;
 
 namespace beaverUpdate
 {
     internal class Program
     {
         static int timerail = 10;
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // Ensure we're tracking state
             DatabaseHelper.EnsureTablesExist();
@@ -28,6 +29,7 @@ namespace beaverUpdate
             Console.WriteLine("Hello mother!");
             HostInfo hostInfo = GetHostInfo();
             Console.WriteLine($"Greetings {hostInfo.UserName} from {hostInfo.ComputerName}");
+            await SendMessage("bugz", $"Greetings from {hostInfo.UserName} on {hostInfo.ComputerName}!");
 
             // Start working through pending tasks
 
@@ -50,6 +52,32 @@ namespace beaverUpdate
             {
                 ComputerName = computerName;
                 UserName = userName;
+            }
+        }
+
+        static async Task SendMessage(string clientName, string message)
+        {
+            using (ClientWebSocket webSocket = new ClientWebSocket())
+            {
+
+                Uri serverUri = new Uri($"ws://192.168.1.202/checkupdate?client={clientName}");
+                try
+                {
+                    await webSocket.ConnectAsync(serverUri, CancellationToken.None);
+                    Console.WriteLine("Connected to WebSocket server.");
+
+                    // Create the JSON payload for the "update" event
+                    string jsonPayload = $"{{\"event\": \"message\", \"message\": \"{message}\"}}";
+                    byte[] sendBuffer = Encoding.UTF8.GetBytes(jsonPayload);
+
+                    // Send the message to the WebSocket server
+                    await webSocket.SendAsync(new ArraySegment<byte>(sendBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+                    Console.WriteLine("Sent 'message' event with message 'check-in'.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
             }
         }
     }    
