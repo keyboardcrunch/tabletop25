cad7b573a169272dd8d22f191aeed1655ace463c
keyboardcrunch <frank@keybaordcrunch>
2025-04-30 19:41:36 -0700

    moved universal stuff to lib

---
+++ BeaverLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BeaverPro")]
+[assembly: AssemblyDescription("Extended Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BeaverPro")]
+[assembly: AssemblyProduct("BeaverNotes")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e4339759-9261-402e-9d79-a0a19d4fdb1e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
